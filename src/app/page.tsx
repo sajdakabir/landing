b57c4708ca050a62af2f2f01ba6315a44fe923cc
Xron Trix <xrontrix@gmail.com>
2024-05-04 11:37:48 +0530

    feat: added request input dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Footer from "@/components/footer/Footer";
 import HomeNavbar from "@/components/header/HomeNavbar";
 import { MoveRight } from "lucide-react";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Home() {
   return (
@@ -27,14 +35,43 @@ export default function Home() {
         </div>
 
         <div className="mt-6">
-          <button className="group mx-auto flex items-center px-3 py-2 text-xs text-text-hover border border-text-hover rounded-full hover:bg-text-hover hover:text-black">
-            Request Access{" "}
-            <MoveRight
-              className="ml-1 group-hover:translate-x-1 transition-transform duration-300"
-              strokeWidth={1.4}
-              size={14}
-            />
-          </button>
+          <Dialog>
+            <DialogTrigger asChild className="outline-none focus:outline-none">
+              <button className="group mx-auto flex items-center px-3 py-2 text-xs text-text-hover border border-text-hover rounded-full hover:bg-text-hover hover:text-black">
+                Request Access{" "}
+                <MoveRight
+                  className="ml-1 group-hover:translate-x-1 transition-transform duration-300"
+                  strokeWidth={1.4}
+                  size={14}
+                />
+              </button>
+            </DialogTrigger>
+            <DialogContent className="bg-dashboard rounded-xl md:rounded-2xl border-none h-fit w-fit md:w-96">
+              <form className="w-fit mx-auto flex flex-col gap-y-2">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="md:mx-auto md:w-72"
+                  id="username"
+                  autoComplete="username"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="md:mx-auto md:w-72"
+                  id="email"
+                  autoComplete="email"
+                />
+
+                <button
+                  type="submit"
+                  className="mt-3 mx-auto flex items-center justify-center px-3 py-2 text-xs text-text-hover border border-text-hover rounded-full hover:bg-text-hover hover:text-black"
+                >
+                  Submit
+                </button>
+              </form>
+            </DialogContent>
+          </Dialog>
           <div className="mt-3">
             <p className="text-xs text-dull-text">
               We&apos;re currently in a private alpha.
