79f4541e555df5b502ad18053759c50241f61695
Xron Trix <xrontrix@gmail.com>
2024-05-07 15:06:01 +0530

    ui: minor changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,9 +3,9 @@ import Heart from "../icons/Heart";
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 flex items-center justify-center md:justify-between w-full h-20">
-      <div className="flex items-center gap-x-2 text-xs md:text-sm">
-        Crafted with <Heart size={14} /> for makers on the internet.
+    <footer className="bottom-0 text-dull-text flex items-center justify-center md:justify-between w-full h-20">
+      <div className="flex items-center gap-x-1 text-xs md:text-sm">
+        Crafted with <Heart size={13} /> for makers on the internet.
       </div>
 
       <div className="hidden md:flex items-center gap-x-3">
